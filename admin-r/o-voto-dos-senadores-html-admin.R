11ad7f799cd1cbbbb1cf19cdf7ca82dca7a76828
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2021-12-02 19:00:45 -0300

    Add files via upload

---
+++ admin-r/o-voto-dos-senadores-html-admin.R
@@ -0,0 +1,82 @@
+# leitura dos pacotes
+library(tidyverse)
+library(rvest)
+
+# URL dos senadores em exercício
+url_1 <- "https://www25.senado.leg.br/web/senadores/em-exercicio/-/e/por-nome"
+
+# URL da votação nominal
+url_2 <- "https://www25.senado.leg.br/web/atividade/materias/-/materia/150731/votacoes#votacao_6475"
+
+# coleta dos partios dos senadores em exercício
+senadores <- url_1 %>%
+  read_html() %>%
+  html_node("#senadoresemexercicio-tabela-senadores")%>%
+  html_table(fill=T) %>%
+  janitor::clean_names()%>%
+  select(nome, partido, uf)%>%
+  mutate(nome_upper = toupper(abjutils::rm_accent(nome)))%>%
+  mutate(partido = case_when(partido == "PODEMOS" ~ "PODE",
+                             TRUE ~ partido))
+
+# coleta dos dados da votação, como nome e voto
+votos_possiveis <- c("Não Compareceu", "Não",  "Sim ", "Abstenção", "Presidente (art.  RISF) ", "- art. , caput - Atividade parlamentar", "Abstenção")
+
+votacao <- url_2 %>%
+  read_html() %>%
+  html_nodes("table.table.table-striped.table-condensed") %>%
+  html_nodes("tbody>tr") %>%
+  html_text() %>%
+  as.data.frame() %>%
+  rename(content = ".") %>%
+  mutate(content = str_remove_all(content, '[0-9]')) %>% 
+  mutate(voto = case_when(str_detect(content, "Sim") ~ "Sim",
+                          str_detect(content, "Não Compareceu") ~ "Não Compareceu",
+                          str_detect(content, "Não registrou voto") ~ "Não registrou voto",
+                          str_detect(content, "Licença particular") ~ "Licença particular",
+                          str_detect(content, "Licença saúde") ~ "Licença saúde",
+                          str_detect(content, "Missão") ~ "Missão",
+                          str_detect(content, "Não") ~ "Não",
+                          str_detect(content, "Abstenção") ~ "Abstenção",
+                          str_detect(content, "Obstrução") ~ "Obstrução",
+                          str_detect(content, "Presidente") ~ "Presidente",
+                          str_detect(content, "Atividade parlamentar") ~ "Atividade parlamentar",
+                          str_detect(content, "Abstenção") ~ "Abstenção",
+                          TRUE ~ "Nova votação")) %>%
+  mutate(content = str_remove_all(content, paste(votos_possiveis, collapse = "|"))) %>%
+  separate(content, c("nome_politico", "delete"), sep = "-") %>%
+  select(-delete) %>%
+  mutate(nome_politico = str_trim(nome_politico),
+         voto = str_trim(voto),
+         nome_upper = toupper(abjutils::rm_accent(nome_politico))) %>%
+  filter(nome_politico != "") %>%
+  mutate(id_politico = row_number(),
+         id_votacao = case_when(id_politico %in% 1:81 ~ "V1", 
+                                id_politico %in% 82:162 ~ "V2", 
+                                id_politico %in% 163:243 ~ "V3", 
+                                TRUE ~ "Na")) %>%
+  mutate(voto = case_when(voto == "Sim" ~ "Sim",
+                          voto == "Missão" ~ "Ausente",
+                          voto == "Não Compareceu" ~ "Ausente",
+                          voto == "Não registrou voto" ~ "Ausente",
+                          voto == "Licença particular" ~ "Ausente",
+                          voto == "Licença saúde" ~ "Ausente",
+                          voto == "Atividade parlamentar" ~ "Ausente",
+                          voto == "Presidente" ~ "Não votou",
+                          voto == "Não" ~ "Não",
+                          voto == "Abstenção" ~ "Abstenção",
+                          voto == "Obstrução" ~ "Obstrução"))
+  
+# cruzamento dos partidos atuais + dados dos votantes
+###### ATENÇÃO: DEFINA ABAIXO QUAL VOTAÇÃO DO LINK VOCê DESEJA
+###### OBS: V1 é a primeira. V2 é a segunda. ETC
+
+votacao_escolhida <- "V3"
+
+votacao_final <- votacao %>%
+  left_join(senadores, by = "nome_upper") %>%
+  filter(id_votacao == votacao_escolhida) %>%
+  select(nome, partido, voto)
+
+# download do CSV padronizado para o ADMIN
+write.csv(votacao_final, paste0("votacao_final_", Sys.Date() ,".csv"), row.names = F)
